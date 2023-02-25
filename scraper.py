77ca3e8e79dc9060bc7b58bb6d567d97f8e18ee8
ACGuerrero <acastillo@ciencias.unam.mx>
2023-02-25 08:16:14 -0600

    added threading

--- scraper.py
+++ scraper.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+import threading
 base_url = 'https://en.wikipedia.org/'
+
+def soupify(current_url):
+     response = requests.get(current_url)
+     return BeautifulSoup(response.content, 'html.parser')
 
 def obtain_paragraphs(current_url):
         # We make a request to the current url
-        response = requests.get(current_url)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        body_content = soup.find('div',attrs={'id':'bodyContent'})
+        body_content = soupify(current_url).find('div',attrs={'id':'bodyContent'})
         main_article = body_content.find('div',attrs={'id':'mw-content-text'})
         return main_article.find_all('p')
 
@@ -23,20 +26,15 @@ def obtain_first_link(paragraphs):
         #If previous lines worked, stop looking
         if len(links) != 0:
              break
-        return links[False]
+    return links[False]
 
-
-def main(start_url):
-    current_url = requests.get(start_url).url
+def steps_to_philosophy(start_url):
     visited_urls = []
+    current_url = requests.get(start_url).url
     visited_urls.append(current_url[len(base_url)-1:])
-
     print('Starting at ', current_url[len(base_url)-1:])
-    i=0
-    while i<50:
-        paragraphs = obtain_paragraphs(current_url)
-        link = obtain_first_link(paragraphs)
-
+    for i in range(50):
+        link = obtain_first_link(obtain_paragraphs(current_url))
         # Check if we just finished (either loop or philosophy)
         if link == '/wiki/Philosophy':
             print(f'It took {i} steps to reach the philosophy page')
@@ -47,8 +45,17 @@ def main(start_url):
         # Update the url to which the next request will be done
         current_url = base_url+link
         visited_urls.append(link)
-        i+=1
 
+#main('https://en.wikipedia.org/wiki/Special:Random')
+def main(start_url):
+    threads = []
+    for _ in range(10):
+        t= threading.Thread(target=steps_to_philosophy,args=[start_url])
+        t.start()
+        threads.append(t)
+    for thread in threads:
+        thread.join()
+    
 
 if __name__ == "__main__":
     random_article_url='https://en.wikipedia.org/wiki/Special:Random' 
